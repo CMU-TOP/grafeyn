2310c460b494b2a5b54e56c94ce1311b6b436b1b
Sam Westrick <shwestrick@gmail.com>
2023-05-16 12:46:43 -0400

    attempt some SKD stuff

--- src/pyser/pyser.py
+++ src/pyser/pyser.py
@@ -2,6 +2,17 @@ from qiskit.circuit import QuantumCircuit
 from qiskit.compiler import transpile
 import sys
 
+# from qiskit.transpiler.passes import SolovayKitaevDecomposition
+from qiskit.synthesis import generate_basic_approximations
+from qiskit.transpiler.passes.synthesis import SolovayKitaev
+
+# https://qiskit.org/documentation/stubs/qiskit.transpiler.passes.SolovayKitaev.html
+def skd(c):
+  basis_gates = ['x', 'y', 'z', 'h', 't']
+  approx = generate_basic_approximations(basis_gates, depth=3)
+  f = SolovayKitaev(recursion_degree=2, basic_approximations=approx)
+  return f(c)
+
 
 def main():
     cfile = sys.argv[1]
