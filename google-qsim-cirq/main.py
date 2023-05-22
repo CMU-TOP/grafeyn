6f9e152d157dce36314b92b01d48521bdd70e056
Sam Westrick <shwestrick@gmail.com>
2023-05-22 15:46:19 -0400

    some updates for cirq output

--- google-qsim-cirq/main.py
+++ google-qsim-cirq/main.py
@@ -5,11 +5,12 @@ import sys
 from cirq.contrib.qasm_import import circuit_from_qasm
 
 if len(sys.argv) < 3:
-    print("usage: python main.py <QASMFILE> <NUM_THREADS>")
+    print("usage: python main.py <QASMFILE> <NUM_THREADS> [<PRINT_OUTPUT?>]")
     sys.exit(1)
 
 filename = sys.argv[1]
 numprocs = int(sys.argv[2])
+doOutput = bool(sys.argv[3]) if len(sys.argv) > 3 else False
 
 f = open(filename)
 circuit = circuit_from_qasm(f.read())
@@ -21,5 +22,20 @@ circuit.append(cirq.measure(*qubits))
 
 opt = qsimcirq.QSimOptions(cpu_threads=numprocs, use_gpu=False)
 qsim_simulator = qsimcirq.QSimSimulator(opt)
-result = qsim_simulator.simulate(circuit)
-print(np.around(result.final_state_vector, 3))
+result = qsim_simulator.simulate(circuit).final_state_vector
+print('simulation completed')
+# result = np.around(result.final_state_vector, 3)
+
+def basisidx(i):
+  maxlen = len(bin(len(result)-1)) - 2
+  bidx = bin(i)[2:]
+  return bidx.rjust(maxlen, '0')
+
+if doOutput:
+  print('computing non-zeros...')
+  ind = np.arange(len(result))
+  nonzeros = ind[result[ind] != 0j]
+  for i in nonzeros:
+    print('|{}⟩ {}'.format(basisidx(i), result[i]))
+else:
+  print('skipping output')
