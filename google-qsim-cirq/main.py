3e6469e68d03352cd9e475f65d8e9037cb4f0a5f
Sam Westrick <shwestrick@gmail.com>
2023-05-22 14:14:53 -0400

    main google cirq with usage

--- google-qsim-cirq/main.py
+++ google-qsim-cirq/main.py
@@ -2,30 +2,24 @@ import cirq
 import qsimcirq
 import numpy as np
 import sys
-
 from cirq.contrib.qasm_import import circuit_from_qasm
 
-# Pick up to ~25 qubits to simulate (requires ~256MB of RAM)
-#qubits = [cirq.GridQubit(i,j) for i in range(2) for j in range(2)]
+if len(sys.argv) < 3:
+    print("usage: python main.py <QASMFILE> <NUM_THREADS>")
+    sys.exit(1)
 
-# Define a circuit to run
-# (Example is from the 2019 "Quantum Supremacy" experiement)
-#circuit = (cirq.experiments.
-#    random_rotations_between_grid_interaction_layers_circuit(
-#    qubits=qubits, depth=16))
+filename = sys.argv[1]
+numprocs = int(sys.argv[2])
 
-f = open(sys.argv[1])
+f = open(filename)
 circuit = circuit_from_qasm(f.read())
-
 print(cirq.qasm(circuit))
 
-# Measure qubits at the end of the circuit
-#circuit.append(cirq.measure(*qubits, key='all_qubits'))
+# TODO: Mike: what does this do? Do we need it?
 qubits = circuit.all_qubits()
 circuit.append(cirq.measure(*qubits))
 
-# Simulate the circuit with qsim and return just the measurement values
-# just like you would with Cirq
-qsim_simulator = qsimcirq.QSimSimulator()
+opt = qsimcirq.QSimOptions(cpu_threads=numprocs, use_gpu=False)
+qsim_simulator = qsimcirq.QSimSimulator(opt)
 result = qsim_simulator.simulate(circuit)
 print(np.around(result.final_state_vector, 3))
