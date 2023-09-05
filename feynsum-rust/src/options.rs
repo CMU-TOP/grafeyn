22d44554021ca94702bf555fc05e1b20e9b26468
Byeongjee Kang <byeongjee.kang@gmail.com>
2023-09-05 16:34:01 -0400

    Improve argument handling

--- feynsum-rust/src/options.rs
+++ feynsum-rust/src/options.rs
@@ -7,12 +7,17 @@ pub struct Options {
     #[structopt(
         parse(from_os_str),
         name = "input",
+        short = "i",
+        long = "input",
         help = "path to the input qasm file"
     )]
     pub input: PathBuf,
+
     #[structopt(
         parse(from_os_str),
         name = "output",
+        short = "o",
+        long = "output",
         help = "output file path to dump densities to. if not specified, densities are not printed"
     )]
     pub output: Option<PathBuf>,
