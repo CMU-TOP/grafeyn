0349686e8a362b207f51e9a8045671a16c41416c
Byeongjee Kang <byeongjee.kang@gmail.com>
2023-09-05 15:49:26 -0400

    Refine help message

--- feynsum-rust/src/options.rs
+++ feynsum-rust/src/options.rs
@@ -4,6 +4,10 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "feynsum", about = "Feynsum quantum simulator")]
 pub struct Options {
-    #[structopt(parse(from_os_str))]
+    #[structopt(
+        parse(from_os_str),
+        name = "input",
+        help = "path to the input qasm file"
+    )]
     pub input: PathBuf,
 }
