c0f68462286245d82c06513208088adba4e9f793
Byeongjee Kang <byeongjee.kang@gmail.com>
2023-09-06 23:47:34 -0400

    Remove unused debug messages

--- feynsum-rust/src/simulator/state_expander.rs
+++ feynsum-rust/src/simulator/state_expander.rs
@@ -1,5 +1,3 @@
-use log::debug;
-
 use crate::circuit::{Gate, MaybeBranchingOutput, PushApplicable};
 
 use super::state::State;
@@ -19,16 +17,13 @@ pub fn expand(
     let mut num_nonzero = 0;
 
     let prev_entries = state.compactify();
-    debug!("prev entries compactified");
 
     let mut table = SparseStateTable::new();
 
     for (bidx, weight) in prev_entries {
         for gate in gates.iter() {
-            debug!("applying gate {:?} to idx {}", gate, bidx);
             match gate.push_apply(&bidx, &weight)? {
                 MaybeBranchingOutput::OuptutOne((new_bidx, new_weight)) => {
-                    debug!("gate applied. one output: ({}, {})", new_bidx, new_weight);
                     table.put(new_bidx, new_weight);
                     num_nonzero += 1;
                 }
@@ -36,10 +31,6 @@ pub fn expand(
                     (new_bidx1, new_weight1),
                     (new_bidx2, new_weight2),
                 ) => {
-                    debug!(
-                        "gate applied. two outputs: ({}, {}), ({}, {})",
-                        new_bidx1, new_weight1, new_bidx2, new_weight2
-                    );
                     table.put(new_bidx1, new_weight1);
                     table.put(new_bidx2, new_weight2);
                     num_nonzero += 2;
