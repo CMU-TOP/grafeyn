1412f62afe42534d31631fb7d5b699d4af1b3e3b
Byeongjee Kang <byeongjee.kang@gmail.com>
2023-09-07 00:32:39 -0400

    Fix bug where gate was not applied recursively

--- feynsum-rust/src/simulator/state_expander.rs
+++ feynsum-rust/src/simulator/state_expander.rs
@@ -1,4 +1,5 @@
 use crate::circuit::{Gate, MaybeBranchingOutput, PushApplicable};
+use crate::types::{BasisIdx, Complex};
 use crate::utility::is_zero;
 
 use super::state::State;
@@ -8,6 +9,31 @@ use super::SimulatorError;
 pub struct ExpandResult {
     pub state: State,
     pub num_nonzero: usize,
+}
+
+fn apply_gates(
+    gates: &[&Gate],
+    table: &mut SparseStateTable,
+    bidx: BasisIdx,
+    weight: Complex,
+) -> Result<(), SimulatorError> {
+    if is_zero(weight) {
+        return Ok(());
+    }
+    if gates.is_empty() {
+        table.put(bidx, weight);
+        return Ok(());
+    }
+
+    match gates[0].push_apply(&bidx, &weight)? {
+        MaybeBranchingOutput::OuptutOne((new_bidx, new_weight)) => {
+            apply_gates(&gates[1..], table, new_bidx, new_weight)
+        }
+        MaybeBranchingOutput::OutputTwo((new_bidx1, new_weight1), (new_bidx2, new_weight2)) => {
+            apply_gates(&gates[1..], table, new_bidx1, new_weight1)?;
+            apply_gates(&gates[1..], table, new_bidx2, new_weight2)
+        }
+    }
 }
 
 pub fn expand(
@@ -20,23 +46,7 @@ pub fn expand(
     let mut table = SparseStateTable::new();
 
     for (bidx, weight) in prev_entries {
-        if is_zero(weight) {
-            continue;
-        }
-        for gate in gates.iter() {
-            match gate.push_apply(&bidx, &weight)? {
-                MaybeBranchingOutput::OuptutOne((new_bidx, new_weight)) => {
-                    table.put(new_bidx, new_weight);
-                }
-                MaybeBranchingOutput::OutputTwo(
-                    (new_bidx1, new_weight1),
-                    (new_bidx2, new_weight2),
-                ) => {
-                    table.put(new_bidx1, new_weight1);
-                    table.put(new_bidx2, new_weight2);
-                }
-            }
-        }
+        apply_gates(&gates, &mut table, bidx, weight)?;
     }
 
     let num_nonzero = table.size();
