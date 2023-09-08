2e2b0e982157b29546258baee20b8f4f2a3a4028
Byeongjee Kang <byeongjee.kang@gmail.com>
2023-09-08 12:36:58 -0400

    Refine table module

--- feynsum-rust/src/simulator/table.rs
+++ feynsum-rust/src/simulator/table.rs
@@ -1,44 +1,5 @@
-use std::collections::HashMap;
+mod dense_state_table;
+mod sparse_state_table;
 
-use crate::types::{BasisIdx, Complex};
-use crate::utility;
-
-#[derive(Debug)]
-pub struct SparseStateTable {
-    table: HashMap<BasisIdx, Complex>,
-}
-
-impl SparseStateTable {
-    pub fn singleton(bidx: BasisIdx, weight: Complex) -> Self {
-        Self {
-            table: HashMap::from([(bidx, weight)]),
-        }
-    }
-
-    pub fn new() -> Self {
-        Self {
-            table: HashMap::new(),
-        }
-    }
-
-    pub fn compactify(self) -> impl Iterator<Item = (BasisIdx, Complex)> {
-        self.table.into_iter()
-    }
-
-    pub fn put(&mut self, bidx: BasisIdx, weight: Complex) {
-        self.table
-            .entry(bidx)
-            .and_modify(|w| *w += weight)
-            .or_insert(weight);
-    }
-
-    pub fn num_nonzero(&self) -> usize {
-        self.table
-            .iter()
-            .filter(|(_, w)| utility::is_nonzero(**w))
-            .count()
-    }
-}
-
-#[derive(Debug)]
-pub struct DenseStateTable {}
+pub use dense_state_table::DenseStateTable;
+pub use sparse_state_table::SparseStateTable;
---
+++ feynsum-rust/src/simulator/table/sparse_state_table.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+
+use crate::types::{BasisIdx, Complex};
+use crate::utility;
+
+#[derive(Debug)]
+pub struct SparseStateTable {
+    table: HashMap<BasisIdx, Complex>,
+}
+
+impl SparseStateTable {
+    pub fn singleton(bidx: BasisIdx, weight: Complex) -> Self {
+        Self {
+            table: HashMap::from([(bidx, weight)]),
+        }
+    }
+
+    pub fn new() -> Self {
+        Self {
+            table: HashMap::new(),
+        }
+    }
+
+    pub fn compactify(self) -> impl Iterator<Item = (BasisIdx, Complex)> {
+        self.table.into_iter()
+    }
+
+    pub fn put(&mut self, bidx: BasisIdx, weight: Complex) {
+        self.table
+            .entry(bidx)
+            .and_modify(|w| *w += weight)
+            .or_insert(weight);
+    }
+
+    pub fn num_nonzero(&self) -> usize {
+        self.table
+            .iter()
+            .filter(|(_, w)| utility::is_nonzero(**w))
+            .count()
+    }
+}
