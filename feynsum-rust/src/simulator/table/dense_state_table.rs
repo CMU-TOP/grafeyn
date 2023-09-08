445d7067ad96886b11b92f49ebc9f914ccec4614
Byeongjee Kang <byeongjee.kang@gmail.com>
2023-09-08 14:10:01 -0400

    Implement DenseStateTable

--- feynsum-rust/src/simulator/table/dense_state_table.rs
+++ feynsum-rust/src/simulator/table/dense_state_table.rs
@@ -1,2 +1,41 @@
+use std::collections::HashMap;
+use std::hash::{BuildHasher, Hasher};
+
+use crate::types::{BasisIdx, Complex};
+
+pub struct TrivialHashser(u64);
+
+impl Hasher for TrivialHashser {
+    fn write(&mut self, _bytes: &[u8]) {
+        unreachable!()
+    }
+    fn write_u64(&mut self, n: u64) {
+        self.0 = n;
+    }
+    fn finish(&self) -> u64 {
+        self.0
+    }
+}
+
+pub struct BuildTrivialHashser;
+
+impl BuildHasher for BuildTrivialHashser {
+    type Hasher = TrivialHashser;
+    fn build_hasher(&self) -> Self::Hasher {
+        TrivialHashser(0)
+    }
+}
+
 #[derive(Debug)]
-pub struct DenseStateTable {}
+pub struct DenseStateTable {
+    table: HashMap<BasisIdx, Complex, BuildTrivialHashser>,
+}
+
+impl DenseStateTable {
+    pub fn new(num_qubits: usize) -> Self {
+        let capacity = 1 << num_qubits;
+        Self {
+            table: HashMap::with_capacity_and_hasher(capacity, BuildTrivialHashser),
+        }
+    }
+}
