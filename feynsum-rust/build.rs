1448c3d2e49ed9f780006a8a98b74e890b816117
Byeongjee Kang <byeongjee.kang@gmail.com>
2023-11-25 16:41:59 -0500

    Add FUTHARK_BACKEND env variable

--- feynsum-rust/build.rs
+++ feynsum-rust/build.rs
@@ -1,7 +1,9 @@
 fn main() {
-    futhark_bindgen::build(
-        futhark_bindgen::Backend::C,
-        "futhark/lib.fut",
-        "futhark_lib.rs",
-    )
+    let futhark_backend = match option_env!("FUTHARK_BACKEND") {
+        None => futhark_bindgen::Backend::C,
+        Some("c") => futhark_bindgen::Backend::C,
+        Some("cuda") => futhark_bindgen::Backend::CUDA,
+        _ => panic!("Unknown FUTHARK_BACKEND"),
+    };
+    futhark_bindgen::build(futhark_backend, "futhark/lib.fut", "futhark_lib.rs")
 }
