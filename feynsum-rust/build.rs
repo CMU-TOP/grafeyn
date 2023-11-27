81f178fdfae642e217e502b148c995b8041d14fa
Byeongjee Kang <byeongjee.kang@gmail.com>
2023-11-27 10:27:39 -0500

    Support futhark multicore backend
    `futhark multicore` translates a Futhark program to a multithreaded C code.

--- feynsum-rust/build.rs
+++ feynsum-rust/build.rs
@@ -2,6 +2,7 @@ fn main() {
     let futhark_backend = match option_env!("FUTHARK_BACKEND") {
         None => futhark_bindgen::Backend::C,
         Some("c") => futhark_bindgen::Backend::C,
+        Some("multicore") => futhark_bindgen::Backend::Multicore,
         Some("cuda") => futhark_bindgen::Backend::CUDA,
         _ => panic!("Unknown FUTHARK_BACKEND"),
     };
