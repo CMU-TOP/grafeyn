f3c989bf9cd50e7b8481f8ac42539c5af265d722
Colin McDonald <colinmcd0731@gmail.com>
2024-01-04 12:29:42 -0500

    Add profiler

--- feynsum-sml/run.sh
+++ feynsum-sml/run.sh
@@ -1,4 +1,4 @@
-./main.mpl @mpl procs 20 set-affinity megablock-threshold 14 cc-threshold-ratio 1.1 collection-threshold-ratio 2.0 max-cc-depth 1 -- -scheduler-max-branching-stride 1  -dense-thresh 1.1 -scheduler $1 -input $2
+$1 @mpl procs 20 set-affinity megablock-threshold 14 cc-threshold-ratio 1.1 collection-threshold-ratio 2.0 max-cc-depth 1 -- -scheduler-max-branching-stride 1 -dense-thresh 1.1 -scheduler $2 -input $3
 # -scheduler-max-branching-stride 1
 #  --scheduler-disable-fusion
 # -dense-thresh 0.75
